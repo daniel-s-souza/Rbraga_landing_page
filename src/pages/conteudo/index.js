602bf0e3cb751fa596899d5d17a3f94a50d8bb7f
daniel-s-souza <danielsantsouza94@gmail.com>
2023-04-03 13:57:02 -0300

    feat: implementa estrutura de conteudos

--- src/pages/conteudo/index.js
+++ src/pages/conteudo/index.js
@@ -3,7 +3,43 @@ import React, { Component } from 'react'
 export default class index extends Component {
   render() {
     return (
-      <div>index</div>
+      <>
+      <section>
+      <div className='modulo1'>
+        <p className='modulo'> Módulo I </p>
+        <p className=''> Não confunda amor com finanças</p>
+        <p>Compreenda a relação do dinheiro no relacionamento a dois e como evitar erros que os casais cometem que destroem a boa relação financeira.</p>
+      </div>
+      <div className='modulo2'>
+        <p className='modulo'> Módulo II </p>
+        <p className=''> Você erra desde o namoro</p>
+        <p>Ainda é tempo de corrigir erros financeiros que começaram nos primeiros dias da relação. Rompa com os erros do passado.</p>
+      </div>
+      <div className='modulo3'>
+        <p className='modulo'> Módulo III </p>
+        <p className=''> Confiança financeira é essencial</p>
+        <p>Como não cair nas armadilhas do consumo e evitar brigar por dinheiro na relação. Como os casais financeiramente felizes fazem?</p>
+      </div>
+      </section>
+      <section>
+      <div className='modulo4'>
+        <p className='modulo'> Módulo IV </p>
+        <p className=''> Vocês se entendem financeiramente?</p>
+        <p>Como tomar decisões financeiras importantes: como evitar a morte financeira na hora de comprar um carro ou a casa própria.</p>
+      </div>
+      <div className='modulo5'>
+        <p className='modulo'> Módulo V </p>
+        <p className=''> Cartão de crédito: amigo ou vilão?</p>
+        <p>Aprenda a avaliar quanto você ganha e a avaliar os gastos do cartão de crédito, evitando as armadilhas do crédito rotativo e do endividamento.</p>
+      </div>
+      <div className='modulo6'>
+        <p className='modulo'> Módulo VI </p>
+        <p className=''> Dinheiro traz felicidade ao casal?</p>
+        <p>Saiba como controlar o orçamento familiar e considerar o dinheiro como instrumento de bem-estar, por meio de simulações de investimentos.</p>
+      </div>
+      </section> 
+    </>
+      
     )
   }
 }
