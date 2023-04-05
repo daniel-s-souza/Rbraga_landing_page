6c2a96a3ee84b40c3d8a8efd86ba5a7bf16da075
daniel-s-souza <danielsantsouza94@gmail.com>
2023-04-05 14:32:07 -0300

    feat: implementa css aboutPage

--- src/pages/AboutPage/index.js
+++ src/pages/AboutPage/index.js
@@ -6,12 +6,12 @@ export default class ola extends Component {
   render() {
     return (
       <div id='sobre'>
-        <img src={img} alt='Robson Braga'/>
-        <h3 className='aboutTitle'>Olá aqui é Robson Braga.</h3>
+        <img className='aboutImg' src={img} alt='Robson Braga'/>
+        <h3 className='aboutTitle'>Olá, aqui é Robson Braga.</h3>
 
-        <p>
-        Sou administrador, especialista em finanças, mestre em Administração pela Universidade Federal da Bahia (UFBA) e doutor em contabilidade e controladoria pela Universidade de São Paulo (USP). Cursei Data Science na Universidade de Salamanca (Espanha).
-        Sou professor de finanças com mais de 20 anos de experiência e, como consultor empresarial já auxiliei dezenas de empresas a organizar sua situação financeira.
+        <p className='aboutText'>
+        Sou administrador, especialista em finanças, <b>mestre em Administração pela Universidade Federal da Bahia (UFBA)</b> e <b></b>doutor em contabilidade e controladoria pela Universidade de São Paulo (USP). Cursei Data Science na Universidade de Salamanca (Espanha).
+        Sou professor de finanças com mais de <b>20 anos de experiência</b> e, como consultor empresarial já auxiliei dezenas de empresas a organizar sua situação financeira.
         Neste curso, vou ajudar você a organizar a sua vida financeira, apresentando métodos práticos de como ter um relacionamento financeiramente feliz. Vamos lá?
         </p>      
       </div>
