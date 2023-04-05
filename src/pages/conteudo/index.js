cc157f4c392511d3c6fc36922cacf55f0bd47ea7
daniel-s-souza <danielsantsouza94@gmail.com>
2023-04-05 13:47:04 -0300

    feat: implementa css de conteudos

--- src/pages/conteudo/index.js
+++ src/pages/conteudo/index.js
@@ -1,41 +1,42 @@
 import React, { Component } from 'react'
+import './style.css';
 
 export default class index extends Component {
   render() {
     return (
-      <div id='conteudo'>
+      <div className='content' id='conteudo'>
           <section>
       <div className='modulo1'>
-        <p className='modulo'> Módulo I </p>
-        <p className=''> Não confunda amor com finanças</p>
-        <p>Compreenda a relação do dinheiro no relacionamento a dois e como evitar erros que os casais cometem que destroem a boa relação financeira.</p>
+        <p className='moduloTitle'> Módulo I </p>
+        <p className='moduloHeadLine'> Não confunda amor com finanças</p>
+        <p className='moduloContent'>Compreenda a relação do dinheiro no relacionamento a dois e como evitar erros que os casais cometem que destroem a boa relação financeira.</p>
       </div>
       <div className='modulo2'>
-        <p className='modulo'> Módulo II </p>
-        <p className=''> Você erra desde o namoro</p>
-        <p>Ainda é tempo de corrigir erros financeiros que começaram nos primeiros dias da relação. Rompa com os erros do passado.</p>
+        <p className='moduloTitleGreyBox'> Módulo II </p>
+        <p className='moduloHeadLine'> Você erra desde o namoro</p>
+        <p className='moduloContent'>Ainda é tempo de corrigir erros financeiros que começaram nos primeiros dias da relação. Rompa com os erros do passado.</p>
       </div>
       <div className='modulo3'>
-        <p className='modulo'> Módulo III </p>
-        <p className=''> Confiança financeira é essencial</p>
-        <p>Como não cair nas armadilhas do consumo e evitar brigar por dinheiro na relação. Como os casais financeiramente felizes fazem?</p>
+        <p className='moduloTitle'> Módulo III </p>
+        <p className='moduloHeadLine'> Confiança financeira é essencial</p>
+        <p className='moduloContent'>Como não cair nas armadilhas do consumo e evitar brigar por dinheiro na relação. Como os casais financeiramente felizes fazem?</p>
       </div>
       </section>
       <section>
       <div className='modulo4'>
-        <p className='modulo'> Módulo IV </p>
-        <p className=''> Vocês se entendem financeiramente?</p>
-        <p>Como tomar decisões financeiras importantes: como evitar a morte financeira na hora de comprar um carro ou a casa própria.</p>
+        <p className='moduloTitleGreyBox'> Módulo IV </p>
+        <p className='moduloHeadLine'> Vocês se entendem financeiramente?</p>
+        <p className='moduloContent'>Como tomar decisões financeiras importantes: como evitar a morte financeira na hora de comprar um carro ou a casa própria.</p>
       </div>
       <div className='modulo5'>
-        <p className='modulo'> Módulo V </p>
-        <p className=''> Cartão de crédito: amigo ou vilão?</p>
-        <p>Aprenda a avaliar quanto você ganha e a avaliar os gastos do cartão de crédito, evitando as armadilhas do crédito rotativo e do endividamento.</p>
+        <p className='moduloTitle'> Módulo V </p>
+        <p className='moduloHeadLine'> Cartão de crédito: amigo ou vilão?</p>
+        <p className='moduloContent'>Aprenda a avaliar quanto você ganha e a avaliar os gastos do cartão de crédito, evitando as armadilhas do crédito rotativo e do endividamento.</p>
       </div>
       <div className='modulo6'>
-        <p className='modulo'> Módulo VI </p>
-        <p className=''> Dinheiro traz felicidade ao casal?</p>
-        <p>Saiba como controlar o orçamento familiar e considerar o dinheiro como instrumento de bem-estar, por meio de simulações de investimentos.</p>
+        <p className='moduloTitleGreyBox'> Módulo VI </p>
+        <p className='moduloHeadLine'> Dinheiro traz felicidade ao casal?</p>
+        <p className='moduloContent'>Saiba como controlar o orçamento familiar e considerar o dinheiro como instrumento de bem-estar, por meio de simulações de investimentos.</p>
       </div>
       </section> 
 
