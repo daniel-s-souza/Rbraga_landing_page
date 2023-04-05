dd07f9a0d6f502a889213d50d9dd79b3a14e7611
daniel-s-souza <danielsantsouza94@gmail.com>
2023-04-05 11:42:31 -0300

    feat: implementa css home page

--- src/pages/homepage/index.js
+++ src/pages/homepage/index.js
@@ -5,19 +5,21 @@ export default class HomePage extends Component {
   render() {
     return (
       <div id='home' className='imgBlock'>
-          <h3 className='h3Line'> Evento Online </h3>
+        <div className='textDiv'>
+        <h3 className='h3Line'> Evento Online </h3>
           <h1 className='h1Text'> 
               Na alegria e na tristeza
               Na saúde e na doença
-              Na riqueza e...
-              Na conta negativa? 
+              Na riqueza e... 
           </h1>
+          <h1 className='h1Text2'>Na conta negativa?</h1>
           <h2 className='h2Text'> 
             Pessoas casadas prosperam mais financeiramente. Se isso não está acontecendo contigo, esse é o curso! 
           </h2>
           <h4 className='h4Line'>
             Lançamento: Dia 08 de abril de 2023 com Robson Braga 
           </h4>
+        </div>
       </div>
     )
   }
